dce4b0d524cddd315e743c1b38d6e43ca71f331b
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2024-05-23 12:11:45 +0530

    Update Area_Peri_of_Circle.sql

--- Area_Peri_of_Circle.sql
+++ Area_Peri_of_Circle.sql
@@ -1,1 +1,13 @@
 --Find area and perimeter of circle (User Defined Procedure).
+set serveroutput on; 
+DECLARE
+	area NUMBER(6, 2) ; 
+	perimeter NUMBER(6, 2) ; 
+	radius NUMBER:=&var; 
+	pi CONSTANT NUMBER(3, 2) := 3.14; 
+BEGIN
+		area := pi * radius * radius; 
+		perimeter := 2 * pi * radius; 
+		dbms_output.Put_line('Area = ' || area); 
+		dbms_output.Put_line(' Perimeter = ' || perimeter); 
+END; 
